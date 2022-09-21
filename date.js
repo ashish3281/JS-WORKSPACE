a1e32e3ecf21f8dd3f02aa91d12e489dfaab4ef1
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2022-09-21 19:30:01 +0530

    Add files via upload

---
+++ date.js
@@ -0,0 +1,17 @@
+const d=new Date();
+x=d.toDateString()
+console.log(x);
+//Date(year,month,date,hours,minutes,second,-)
+const a=new Date("October 13, 2014 11:13:00");
+console.log(a);
+//get method
+// getFullYear()
+// getMonth()
+// getDay()
+// getHours()
+// getMinutes()
+// getSeconds()
+// getMillisecond()
+const w=new Date();
+w.setFullYear(2020);
+console.log(w);
