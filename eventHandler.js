622377ac057c76e6a84e476693665a143e02eb46
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2022-09-21 19:31:45 +0530

    Add files via upload

---
+++ eventHandler.js
@@ -0,0 +1,24 @@
+// first method
+//     <button onclick="document.getElementById('demo').innerHTML=Date()">the time is</button>
+//     <p id="demo"></p>
+
+// second method
+//      <button onlick="this.innerHTML=Date()">the time is</button>
+
+// third method
+// <script>
+//     function displayDate(){
+//         document.getElementById("demo").innerHTML=Date();
+//     }
+//     </script>
+//     <button onlick="displayDate()">The time is?</button>
+//     <p id="demo"></p>
+
+
+// methods
+// onchange
+// onclick
+// onmouseover
+// onmouseout
+// onkeydown
+// onload
