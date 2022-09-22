5ef48fd66cc0edbb1223fbc09411ffcd1076662f
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2022-09-22 21:54:45 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,34 @@
+// dom is an api for html document
+// it is a way to definethe webpage in a hierachical way to become easy to use and manipulate
+// it represent the entire document as nodes and Object
+// D=document
+// O=Object
+// M=model
+
+
+//window object in js
+var a= window;
+console.log(a);
+//go to the html webpage the consol where you see window and click on it .there you will the method of the window like alert ,prompt,confirm and many more
+// window.alert("hello");
+// console.log(a);
+
+
+// var x=prompt("enter the value of x");
+// var y=prompt("enter the value of y");
+// console.log(x+y);
+//prompt method work as a object x=2 y=3; output will be 23
+
+// var x=parseInt(prompt("enter the value of x"));
+// var y=parseInt( prompt("enter the value of y"));
+// console.log(x+y);
+ //now the is 5
+
+//  a=confirm("are you sure");
+//  console.log(a);
+
+// var a=window.document;
+// console.log(a);
+
+// var a=window.innerHeight;
+// console.log(a);
