0ff98d75fc604b6740fd05868ba2882d89ed2e94
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2022-09-22 21:55:08 +0530

    Add files via upload

---
+++ dom2.js
@@ -0,0 +1,16 @@
+//DOM Manipulate
+
+
+// function button(){
+//     alert("welcome to my webpage");
+// }
+// button();
+
+// function button(){
+//     document.getElementById("heading").innerHTML= "How are you";
+// }
+
+function button(){
+    var c= document.getElementById("heading").innerHTML;
+    alert(c);
+}
