8f9362cc6815fceae96772fad993444c13f22809
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2022-09-22 21:55:24 +0530

    Add files via upload

---
+++ dom3.js
@@ -0,0 +1,33 @@
+// var x = document;
+// console.log(x);
+
+// var x= document.all;
+// console.log(x);
+
+// var x= document.head;
+// console.log(x);
+
+// var x= document.body;
+// console.log(x);
+
+// var x= document.forms;
+// console.log(x);
+
+// var x=document.images;
+// console.log(x);
+
+/////////////////////////////////////////////////
+//query selector method
+
+// var x=document;
+// x=document.querySelector('.left');
+//console.log(x) null
+//x=document.form[0]
+//console.log(x); undefined
+
+function OneS(){
+   var x=document;
+ x=document.querySelector('.left');
+
+ console.log(x);//use onload method
+}
