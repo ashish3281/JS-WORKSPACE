42e877bafc8521ce16ba3fcb4cb782ba007fd777
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2022-09-24 20:40:35 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,35 @@
+console.log("hello");
+var a=15;
+var b=20;
+var c=a+b;
+console.log("the of value is:",c);
+var bikename;
+console.log(bikename);//undefined
+// let car="swift";
+// console.log(car);
+// let car=odi;
+// console.log(car);
+const x=5,y=3;
+const z=x+y;
+console.log(z);
+// const x=2; can not be declared again 
+var p;
+console.log(typeof(p));
+var a=16;
+console.log(typeof(a));
+var t=null;
+console.log(typeof(t));
+var str="hello";
+console.log(str,typeof(str));
+//conversion
+var num=String(34);
+console.log(num,typeof(num));
+
+var str1=Number("56");
+console.log(str1,typeof(str1));
+var bool=Number(true);
+console.log(bool,typeof(bool));
+var number=parseInt('676');
+console.log(number,typeof(number));
+var num=parseFloat(74.765);
+console.log(num.toFixed(2),typeof(num));
