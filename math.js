c824a88262a23034709ea1772861ae2a625ae794
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2022-09-26 18:46:20 +0530

    Add files via upload

---
+++ math.js
@@ -0,0 +1,18 @@
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.6));//rond up value
+console.log(Math.floor(4.6));//round down value
+console.log(Math.trunc(4.6));//return integer value
+console.log(Math.sign(2));
+console.log(Math.sign(-2));
+console.log(Math.pow(2,3));
+console.log(Math.sqrt(64));
+console.log(Math.abs(-2.3));
+console.log(Math.sin(90* Math.PI/180));
+console.log(Math.cos(0*Math.PI/180));
+console.log(Math.random());//return number lower then one
+console.log(Math.min(2,23,242,1));
+console.log(Math.max(12,23,2,3324,433,8787));
+console.log(Math.log(2));
+console.log(Math.log2(8));
+//return a random integer from 0 to 99
+console.log(Math.random()*100);
