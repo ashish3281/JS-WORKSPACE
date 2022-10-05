3d174b763bafd2898322d8c73ea349095e2df908
Ashish_Goyal <92047366+ashish3281@users.noreply.github.com>
2022-10-05 15:29:58 +0530

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,32 @@
+// regular expression in js
+
+//exec() give the index value
+// var regex= /hello/;
+// var str1= " hello welcome to a youtube chennel say hello if you are new one";
+// var output =regex.exec(str1);
+// console.log(output);
+//regular expression in jave is case sensitive
+//if we use /hello/i so it work in all case upper as well as lower
+// if we use /hello/g is global word if there is multiple hello in string so it give all the hello word index
+// output =regex.exec(str1);
+// console.log(output);
+
+
+  //test() it check the word is present in string or not
+//   var regex= /ello/g;
+// var str1= " hello welcome to a youtube chennel say hello if you are new one";
+// var output =regex.test(str1);
+// console.log(output);
+
+
+//match()
+// var regex= /hello/g;
+// var str1= " hello welcome to a youtube chennel say hello if you are new one";
+// var output=str1.match(regex);
+// console.log(output);
+
+//replace()
+// var regex= /hello/g;
+// var str1= " hello welcome to a youtube chennel say hello if you are new one";
+// var output=str1.replace(regex,"hi");
+// console.log(output);
